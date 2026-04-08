b3de7667798a0d33ee4726fce7352a355e2b5b9f
BYOUL JUNG <jbjb7458@gmail.com>
2026-04-08 22:26:31 +0900

    Title: 특정 원소들의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/특정 원소들의 합/sum-of-specific-elements.py
+++ 260408/특정 원소들의 합/sum-of-specific-elements.py
@@ -1,2 +1,9 @@
-arr = [list(map(int, input().split()) for _ in range(4)]
-print(arr)
+arr = [list(map(int, input().split())) for _ in range(4)]
+# print(arr)
+
+sum = 0
+for i in range(4):
+    for j in range(i+1):
+        sum += arr[i][j]
+
+print(sum)
