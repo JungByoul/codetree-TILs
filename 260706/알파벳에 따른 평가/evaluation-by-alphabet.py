a8926c5a3bebab52975666a77c58ae6e3fe18384
BYOUL JUNG <jbjb7458@gmail.com>
2026-07-06 20:56:34 +0900

    [Passed] 알파벳에 따른 평가 (46ms, 16MB)

---
+++ 260706/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,9 @@
+Str = input()
+
+if Str == 'S': print('Superior')
+elif Str =='A': print('Excellent')
+elif Str =='B': print('Good')
+elif Str =='C': print('Usually')
+elif Str =='D': print('Effort')
+else:
+    print('Failure')
